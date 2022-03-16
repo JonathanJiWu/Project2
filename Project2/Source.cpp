577d9463ffed86e8fdad06dcb73429ad487e72b3
JJW <jonathanwubrave@gmail.com>
2022-03-15 22:40:58 -0400

    inheritance

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -1,6 +1,6 @@
 //OOP: Encapsulation: default privet, public, protected
 //abstraction: what's virtual class? what's virtual function?
-//
+//inheritance: 
 #include <iostream>
 
 class iEmloyee//this is the virtual class
@@ -14,7 +14,7 @@ public:
 	std::string Name = "jiji";
 	int Age = 0;
 
-	Employee();
+	Employee();//1.same name as the class 2.need to be public 3. no return type
 	Employee(std::string name, int age)
 	{
 		Name = name;
@@ -27,9 +27,30 @@ public:
 		std::cout << Age<<std::endl;
 	}
 };
+class Developer : Employee
+{
+public:
+	std::string ProgramLanguage;
+
+	Developer();
+	Developer(std::string name, int age, std::string progLan)
+		:Employee(name, age)
+	{
+		ProgramLanguage = progLan;
+	}
+
+	void Cout()
+	{
+		saySomething();
+		std::cout << ProgramLanguage << std::endl;
+	}
+};
+
 
 int main()
 {
 	Employee employee1 = Employee("Lulu Lee", 30);
 	employee1.saySomething();
+	Developer dev1 = Developer("sisi", 55, "java");
+	dev1.Cout();
 }
